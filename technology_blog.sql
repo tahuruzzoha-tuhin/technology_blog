168fbcfc1d1d1e142ca783c6826faad2d6a2efc3
tahuruzzoha <tzz.tuhin@gmail.com>
2020-07-22 07:41:56 +0600

    Database Update

--- technology_blog.sql
+++ technology_blog.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 10, 2020 at 11:06 AM
--- Server version: 10.3.17-MariaDB
--- PHP Version: 7.2.24
+-- Generation Time: Jul 22, 2020 at 03:34 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `blog1`
+-- Database: `techblog`
 --
 
 -- --------------------------------------------------------
@@ -83,14 +82,14 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(9, '2014_10_12_000000_create_users_table', 1),
-(10, '2014_10_12_100000_create_password_resets_table', 1),
-(11, '2020_07_07_131126_create_posts_table', 1),
-(12, '2020_07_07_131334_create_categories_table', 1),
-(13, '2020_07_07_131558_create_tags_table', 1),
-(14, '2020_07_07_131702_create_post_tag_table', 1),
-(15, '2020_07_07_131909_create_comments_table', 1),
-(16, '2020_07_07_133429_add_columns_to_user', 1);
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2020_07_07_131126_create_posts_table', 1),
+(4, '2020_07_07_131334_create_categories_table', 1),
+(5, '2020_07_07_131558_create_tags_table', 1),
+(6, '2020_07_07_131702_create_post_tag_table', 1),
+(7, '2020_07_07_131909_create_comments_table', 1),
+(8, '2020_07_07_133429_add_columns_to_user', 1);
 
 -- --------------------------------------------------------
 
@@ -103,13 +102,6 @@ CREATE TABLE `password_resets` (
   `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `password_resets`
---
-
-INSERT INTO `password_resets` (`email`, `token`, `created_at`) VALUES
-('mehejabin.tisha@gmail.com', '$2y$10$AUV5bGk61NVJunBEhF.rF.L1zTtWCtWozi0cUz8YV7cn77x237P1u', '2020-07-09 23:03:33');
 
 -- --------------------------------------------------------
 
@@ -282,7 +274,7 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `posts`
